b650b53be19f198bd6a36770a5433167da2bc89a
John M Calhoun <johnmcalhoun123@gmail.com>
2018-01-02 22:57:42 -0500

    get region from instance metadata in test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,11 @@
 #! /bin/bash 
 
 cd ..
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | $(npm bin)/jq --raw-output ".region" )
 if [ ! -f ./config.js ]; then 
-    node config.js.example john@example.com > config.json
+    node config.js.example john@example.com $region > config.json
 fi
-
+npm run bootstrap
 if $(aws s3 ls); then
     echo "aws cli configured"
 else
@@ -14,8 +15,8 @@ else
         echo "please attach role to ec2 instance"
     fi
     creds=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/${role_name})
+    
     profile=$(cat config.json | $(npm bin)/jq --raw-output ".profile")
-    region=$(cat config.json | $(npm bin)/jq --raw-output ".region")
     
     aws configure set aws_access_key_id $( echo $creds | $(npm bin)/jq --raw-output ".AccessKeyId")
     aws configure set aws_secret_access_key $( echo $creds | $(npm bin)/jq --raw-output ".SecretAccessKey")
