95c0696784f12ab3f7a7b7d4906294b987939555
Mike <mike@andoverlab.co.uk>
2018-01-18 16:07:15 +0000

    Stopped CognitoRole from being hard coded to us-east-1

--- lambda/cfn/lib/CognitoRole.js
+++ lambda/cfn/lib/CognitoRole.js
@@ -10,7 +10,7 @@ module.exports=class CognitoRole extends require('./base') {
         var RoleMappings={}
 
         params.RoleMappings.map(function(x){
-            var id="cognito-idp.us-east-1.amazonaws.com/"+x.UserPool+':'+x.ClientId
+            var id="cognito-idp."+(process.env.AWS_REGION || "us-east-1")+".amazonaws.com/"+x.UserPool+':'+x.ClientId
             delete x.ClientId
             delete x.UserPool
             RoleMappings[id]=x
