6e2b07cfc2f0422cb325c9379c1a900b986b3c95
John M Calhoun <johnmcalhoun123@gmail.com>
2018-01-21 15:18:13 -0500

    cleaned up website test docker start

--- website/test/start-docker.sh
+++ website/test/start-docker.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-docker run -d -p 4444:4444 --shm-size=2g selenium/standalone-chrome:3.8.1-erbium
+STATUS=$(curl localhost:4444/wd/hub/status | ../../bin/json.js value.ready 2> /dev/null)
+if [ "$STATUS" == "true" ];then
+    echo "ready"
+else
+    echo "starting"
+    docker run -d -p 4444:4444 --shm-size=2g selenium/standalone-chrome:3.8.1-erbium
+fi
