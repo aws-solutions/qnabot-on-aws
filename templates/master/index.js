c7737680d9e25b2e0c8d9d6ae259f3d5d56ada2f
John M Calhoun <johnmcalhoun123@gmail.com>
2018-03-08 15:09:42 -0500

    removed default email address

--- templates/master/index.js
+++ templates/master/index.js
@@ -112,8 +112,7 @@ module.exports={
         "Type":"String",
         "Description":"Email address for the admin user. Will be used for loging in and for setting the admin password. This email will receive the temporary password for the admin user.",
         "AllowedPattern":".+\\@.+\\..+",
-        "ConstraintDescription":"Must be valid email address eg. johndoe@example.com",
-        "Default":"Johndoe@example.com"
+        "ConstraintDescription":"Must be valid email address eg. johndoe@example.com"
     },
     "Username":{
         "Type":"String",
