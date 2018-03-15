293257e2389c78ef169850c5f3fe59a4ac11baea
EC2 Default User <ec2-user@ip-172-31-39-6.ec2.internal>
2018-03-15 14:41:28 +0000

    changing firehose role to actually work

--- templates/master/elasticsearch/metricfirehose.js
+++ templates/master/elasticsearch/metricfirehose.js
@@ -77,29 +77,108 @@ module.exports={
         "PolicyDocument" : {
           "Version": "2012-10-17",
           "Statement": [
-              {
-                "Effect": "Allow",
-                "Action": [
-                  "s3:PutObject"
-                ],
-                "Resource": [
-                  {"Fn::GetAtt": ["MetricsBucket", "Arn"]}
-                ]
-              },
-              {
-                "Effect": "Allow",
-                "Action": [
-                  "es:ESHttpPut",
-                  "es:ESHttpPost"
-                ],
-                "Resource": [
-                  {"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]}
-                ]
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "s3:AbortMultipartUpload",
+                "s3:GetBucketLocation",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads",
+                "s3:PutObject"
+              ],
+              "Resource": [
+                {"Fn::GetAtt": ["MetricsBucket", "Arn"]},
+                {"Fn::Join":["",[{"Fn::GetAtt": ["MetricsBucket", "Arn"]},"/*"]]}
+              ]
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "lambda:InvokeFunction",
+                "lambda:GetFunctionConfiguration"
+              ],
+              "Resource":[
+                {"Fn::Join": ["",["arn:aws:lambda:",{ "Ref" : "AWS::Region" },":",{ "Ref" : "AWS::AccountId" },":function:%FIREHOSE_DEFAULT_FUNCTION%:%FIREHOSE_DEFAULT_VERSION%"]]}
+              ]
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "es:DescribeElasticsearchDomain",
+                "es:DescribeElasticsearchDomains",
+                "es:DescribeElasticsearchDomainConfig",
+                "es:ESHttpPost",
+                "es:ESHttpPut"
+              ],
+              "Resource": [
+                {"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/*"]]}
+              ]
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "es:ESHttpGet"
+              ],
+              "Resource": [
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_all/_settings"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_cluster/stats"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/metrics*/_mapping/feedback"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_nodes"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_nodes/stats"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_nodes/*/stats"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/_stats"]]},
+                {"Fn::Join":["",[{"Fn::GetAtt" : ["ElasticsearchDomain", "DomainArn"]},"/metrics*/_stats"]]}
+              ]
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "logs:PutLogEvents"
+              ],
+              "Resource": [
+                {"Fn::Join": ["",["arn:aws:logs:",{ "Ref" : "AWS::Region" },":",{ "Ref" : "AWS::AccountId" },":log-group:/aws/kinesisfirehose/test:log-stream:*"]]}
+              ]
+            },
+            {
+              "Sid": "",
+              "Effect": "Allow",
+              "Action": [
+                "kinesis:DescribeStream",
+                "kinesis:GetShardIterator",
+                "kinesis:GetRecords"
+              ],
+              "Resource": [
+                {"Fn::Join": ["",["arn:aws:kinesis:",{ "Ref" : "AWS::Region" },":",{ "Ref" : "AWS::AccountId" },":stream/%FIREHOSE_STREAM_NAME%"]]}
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "kms:Decrypt"
+              ],
+              "Resource": [
+                "arn:aws:kms:region:accountid:key/%SSE_KEY_ARN%"
+              ],
+              "Condition": {
+                "StringEquals": {
+                  "kms:ViaService": "kinesis.%REGION_NAME%.amazonaws.com"
+                },
+                "StringLike": {
+                  "kms:EncryptionContext:aws:kinesis:arn": "arn:aws:kinesis:%REGION_NAME%:507296099304:stream/%FIREHOSE_STREAM_NAME%"
+                }
               }
+            }
           ]
         },
         "PolicyName" : "PutQnAMetricsFirehose",
-        "Roles" : [{"Ref":"FirehoseESS3Role"}],
+        "Roles" : [{"Ref":"FirehoseESS3Role"}]
       }
     }
 }
