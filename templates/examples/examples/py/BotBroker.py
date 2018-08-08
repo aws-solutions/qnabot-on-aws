88e9d1f3c4765c92690fadc66d494f1dc76967f8
Arun Donti <ec2-user@ip-172-31-66-254.ec2.internal>
2018-08-08 15:14:31 +0000

    designer can now choose whether or not a new session is created with the specialized bot whenever it is called

--- templates/examples/examples/py/BotBroker.py
+++ templates/examples/examples/py/BotBroker.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import json
 import string
 import boto3
+import time
 import os
 import collections
 from collections import defaultdict
@@ -23,6 +24,7 @@ def handler(event, context):
         event["res"]["session"].pop("queryLambda",None)
         event["res"]["session"].pop("botName",None)
         event["res"]["session"].pop("botAlias",None)
+        event["res"]["session"].pop("brokerUID",None)
         plaintextResp = 'Welcome back to QnABot!!!'
         htmlResp = '<i> Welcome back to QnABot!!! </i>'
         event["res"]["message"] = '{0}'.format(plaintextResp)
@@ -37,13 +39,20 @@ def middleman(event):
     lexClient = boto3.client('lex-runtime')
     tempBotName = event["req"]["_event"].get("sessionAttributes").get("botName" , None)
     tempBotAlias = event["req"]["_event"].get("sessionAttributes").get("botAlias", None)
+    tempBotUserID = event["req"]["_event"].get("sessionAttributes").get("brokerUID", None)
+
     if tempBotName == None:
         tempBotName = event["res"]["result"]["args"][0]
         tempBotAlias = event["res"]["result"]["args"][1]
+        if len(event["res"]["result"]["args"]) < 3 or event["res"]["result"]["args"][2].lower() == "remember":
+            tempBotUserID = event["req"]["_event"]["userId"]
+        else:
+            tempBotUserID ='{0}{1}'.format(event["req"]["_event"]["userId"],int(round(time.time() * 1000)))
+    print (tempBotUserID)       
     response = lexClient.post_text(
         botName = tempBotName,
         botAlias = tempBotAlias,
-        userId= event["req"]["_event"]["userId"],
+        userId= tempBotUserID,
         sessionAttributes= event["req"]["_event"]["sessionAttributes"],
         inputText=event["req"]["question"]
     )
@@ -66,8 +75,9 @@ def middleman(event):
             if 'imageUrl' in card:
                 event["res"]["card"]["imageUrl"] = card["imageUrl"]
     if "botName" not in event["res"]["session"]:            
-        event["res"]["session"]["botName"] = event["res"]["result"]["args"][0]
-        event["res"]["session"]["botAlias"] = event["res"]["result"]["args"][1]
+        event["res"]["session"]["botName"] = tempBotName
+        event["res"]["session"]["botAlias"] = tempBotAlias
+        event["res"]["session"]["brokerUID"] = tempBotUserID
     event["res"]["session"]["queryLambda"] = os.environ['AWS_LAMBDA_FUNCTION_NAME']            
     return event
     
