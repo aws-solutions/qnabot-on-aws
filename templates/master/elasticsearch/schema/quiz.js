a6958d9b455ccfe31b34e786a2108e567db46ae1
Arun Donti <ec2-user@ip-172-31-66-254.ec2.internal>
2018-08-17 16:31:11 +0000

    initial attempt at removing the 'no' stopword using a custom analyzer

--- templates/master/elasticsearch/schema/quiz.js
+++ templates/master/elasticsearch/schema/quiz.js
@@ -108,21 +108,59 @@ module.exports={
             required:["qid","question","incorrectAnswers","correctAnswers"]
         }
     },
+    settings: {
+        analysis: {
+          filter: {
+            english_stop: {
+              type:       "stop",
+              stopwords:  ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it",
+                            "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these",
+                            "they", "this", "to", "was", "will", "with"
+                          ]  
+            },
+            english_keywords: {
+              type:       "keyword_marker",
+              keywords:   ["example"] 
+            },
+            english_stemmer: {
+              type:       "stemmer",
+              language:   "english"
+            },
+            english_possessive_stemmer: {
+              type:       "stemmer",
+              language:   "possessive_english"
+            }
+          },
+          analyzer: {
+            custom_english: {
+              type: "custom",    
+              tokenizer:  "standard",
+              filter: [
+                "english_possessive_stemmer",
+                "lowercase",
+                "english_stop",
+                "english_keywords",
+                "english_stemmer"
+              ]
+            }
+          }
+        }
+    },
     properties:{
         qid:{
             type:"keyword"
         },
         question:{
             type:"string",
-            analyzer:"english"
+            analyzer:"custom_english"
         },
         incorrectAnswers:{
             type:"text",
-            analyzer:"english"
+            analyzer:"custom_english"
         },
         correctAnswers:{
             type:"text",
-            analyzer:"english"
+            analyzer:"custom_english"
         }
     }
 }
