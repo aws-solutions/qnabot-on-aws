a1ad0d3b7278b14610f948a40375490f74897366
Arun Donti <ec2-user@ip-172-31-66-254.ec2.internal>
2018-08-17 18:11:26 +0000

    moved custom analyzer to correct file

--- templates/master/elasticsearch/schema/qna.js
+++ templates/master/elasticsearch/schema/qna.js
@@ -103,44 +103,6 @@ module.exports={
             required:["qid","q","a"]
         }
     },
-    settings: {
-        analysis: {
-          filter: {
-            english_stop: {
-              type:       "stop",
-              stopwords:  ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it",
-                            "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these",
-                            "they", "this", "to", "was", "will", "with"
-                          ]  
-            },
-            english_keywords: {
-              type:       "keyword_marker",
-              keywords:   ["example"] 
-            },
-            english_stemmer: {
-              type:       "stemmer",
-              language:   "english"
-            },
-            english_possessive_stemmer: {
-              type:       "stemmer",
-              language:   "possessive_english"
-            }
-          },
-          analyzer: {
-            custom_english: {
-              type: "custom",    
-              tokenizer:  "standard",
-              filter: [
-                "english_possessive_stemmer",
-                "lowercase",
-                "english_stop",
-                "english_keywords",
-                "english_stemmer"
-              ]
-            }
-          }
-        }
-    },
     properties:{
         qid:{
             type:"keyword"
