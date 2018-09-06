a55738f83ddb25881fc355b7024ba8a2b0536eb1
Arun Donti <ec2-user@ip-172-31-66-254.ec2.internal>
2018-09-06 18:16:51 +0000

    added safety check to caching middleware, should fix issue where client could potentially error out on returning first response to user

--- lambda/fulfillment/lib/middleware/5_cache.js
+++ lambda/fulfillment/lib/middleware/5_cache.js
@@ -7,7 +7,9 @@ var util=require('./util')
 module.exports=async function cache(req,res){
     console.log("Entering Cache Middleware")
     console.log("response:" + JSON.stringify(res))
-    res.session.cachedOutput= res.response
+    if(_.has(res,"out.response")){
+        res.out.sessionAttributes.cachedOutput= res.out.response
+    }
     console.log("edited response:" + JSON.stringify(res))
     return {req,res}
 }
