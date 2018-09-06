f09b8001eb6c6ae8fc1ae4be768b1e54dbf7d83a
John M Calhoun <johnmcalhoun123@gmail.com>
2018-09-05 21:12:56 -0500

    fix bug in cache middleware

--- lambda/fulfillment/lib/middleware/5_cache.js
+++ lambda/fulfillment/lib/middleware/5_cache.js
@@ -7,7 +7,7 @@ var util=require('./util')
 module.exports=async function cache(req,res){
     console.log("Entering Cache Middleware")
     console.log("response:" + JSON.stringify(res))
-    res.out.sessionAttributes.cachedOutput= res.out.response
+    res.session.cachedOutput= res.response
     console.log("edited response:" + JSON.stringify(res))
     return {req,res}
 }
