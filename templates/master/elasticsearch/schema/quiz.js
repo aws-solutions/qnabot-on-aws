1593d2d3739151a0beffa4c37ed59474b3a93a2e
Arun Donti <ec2-user@ip-172-31-66-254.ec2.internal>
2018-10-03 15:27:06 +0000

    Ordering now works with Advanced sub menu

--- templates/master/elasticsearch/schema/quiz.js
+++ templates/master/elasticsearch/schema/quiz.js
@@ -8,13 +8,15 @@ module.exports={
                     type:"string",
                     title:"Quiz Question ID",
                     description:"Assign a unique identifier for this item.  This should not be the same as any other question or document's.",
-                    maxLength:100
+                    maxLength:100,
+                    propertyOrder:0
                 },
                 question:{
                     type:"string",
                     title:"Question",
                     description:"Enter a question that the bot will ask the user.",
                     maxLength:140,
+                    propertyOrder:1
                 },
                 correctAnswers:{
                     title:"Correct Answers",
@@ -24,7 +26,8 @@ module.exports={
                         title:"Correct Answer",
                         type:"string",
                         maxLength:140
-                    }
+                    },
+                    propertyOrder:2
                 },
                 incorrectAnswers:{
                     title:"Incorrect Answers",
@@ -34,7 +37,15 @@ module.exports={
                         title:"Answer",
                         type:"string",
                         maxLength:140
-                    }
+                    },
+                    propertyOrder:3
+                },
+                quiz:{
+                    type:"string",
+                    title:"Quiz ID",
+                    description:"ID of the quiz this question is a member of",
+                    maxLength:100,
+                    propertyOrder:4
                 },
                 responses:{
                     title:"Custom Responses",
@@ -45,21 +56,25 @@ module.exports={
                             title:"Correct Answer Response",
                             description:"Response that will be returned to the user on a correct response.",
                             type:"string",
-                            maxLength:140
+                            maxLength:140,
+                            propertyOrder:0
                         },
                         incorrect:{
                             title:"Incorrect Answer Response",
                             description:"Response that will be returned to the user on a incorrect response.",
                             type:"string",
-                            maxLength:140
+                            maxLength:140,
+                            propertyOrder:1
                         },
                         end:{
                             title:"End Quiz Response",
                             description:"The response that will be returned to the user end the end of the quiz if this is the last question",
                             type:"string",
-                            maxLength:140
+                            maxLength:140,
+                            propertyOrder:2
                         }
-                    }
+                    },
+                    propertyOrder:5
                 },
                 next:{
                     title:"Next Questions",
@@ -69,7 +84,8 @@ module.exports={
                         title:"nextQuestion",
                         type:"string",
                         maxLength:100
-                    }
+                    },
+                    propertyOrder:6
                 },
                 r:{
                     title:"Response card",
@@ -80,17 +96,27 @@ module.exports={
                             type:"string",
                             title:"Card Title",
                             description:"Required",
-                            maxLength:80
+                            maxLength:80,
+                            propertyOrder: 0
                         },
                         subTitle:{
                             type:"string",
                             title:"Card Subtitle",
                             description:"Optional",
-                            maxLength:80
+                            maxLength:80,
+                            propertyOrder: 1
+                        },
+                        imageUrl:{
+                            type:"string",
+                            description:"Optional",
+                            title:"Card Image Url",
+                            format:'url',
+                            maxLength:2000,
+                            propertyOrder: 2
                         },
                         buttons:{
                             title:"Lex Buttons",
-                            description:"Optional. Add response buttons users can click on if they are interacting through Lex.",
+                            description:"Conditionally Required for Lex if no Card Image Url is specified. Add response buttons users can click on if they are interacting through Lex.",
                             type:"array",
                             items:{
                                 title:"Button",
@@ -98,31 +124,22 @@ module.exports={
                                 properties:{
                                     text: {
                                         title: "Display Text",
-                                        type : "string"
+                                        type : "string",
+                                        propertyOrder: 0
                                     },
                                     value: {
                                         title: "Button Value",
-                                        type : "string"
+                                        type : "string",
+                                        propertyOrder: 1
                                     }
                                 },
                                 required:["text","value"]
-                            }   
-                        },
-                        imageUrl:{
-                            type:"string",
-                            description:"Optional",
-                            title:"Card Image Url",
-                            format:'url',
-                            maxLength:2000
+                            },   
+                            propertyOrder: 3
                         }
                     },
+                    propertyOrder:7,
                     required:["title"]
-                },
-                quiz:{
-                    type:"string",
-                    title:"Quiz ID",
-                    description:"ID of the quiz this question is a member of",
-                    maxLength:100,
                 }
             },
             required:["qid","question","incorrectAnswers","correctAnswers"]
--- website/js/components/designer/input.vue
+++ website/js/components/designer/input.vue
@@ -115,66 +115,66 @@ module.exports={
         return title
       }
     },
+    // properties:function(){
+    //   var self=this
+    //   function helperSort(nextDepth){
+    //     return Object.keys(nextDepth)
+    //     .filter(x=>Object.keys(self.value).includes(x))
+    //     .filter(x=>nextDepth.pick ? nextDepth.pick.includes(x) : true) 
+    //     .filter(x=>{
+    //       return nextDepth.omit ? !nextDepth.omit.includes(x) : true
+    //     }) 
+    //     .map(function(x){
+    //       var out=_.cloneDeep(nextDepth[x])
+    //       out.name=x
+    //       return out
+    //     })
+    //     .sort((x,y)=>{
+    //       return _.get(x,'propertyOrder',Number.MAX_SAFE_INTEGER)-_.get(y,'propertyOrder',Number.MAX_SAFE_INTEGER)
+    //     })
+    //   }
+    //   function recurseDown(currentDepth){
+    //       if(_.has(currentDepth,"properties")){
+    //         for(var key in Object.keys(currentDepth.properties)){
+    //           recurseDown(key)
+    //         }
+    //         return helperSort(currentDepth.properties)
+    //       }
+    //       if(_.has(currentDepth,"items.properties")){
+    //         for(var key in Object.keys(currentDepth.items.properties)){
+    //           recurseDown(key)
+    //         }
+    //         return helperSort(currentDepth.properties)
+    //       }
+    //     return currentDepth
+    //   } 
+    //   if(this.schema.properties){
+    //     return recurseDown(this.schema)
+    //   }else{
+    //     []
+    //   }
+    // },
     properties:function(){
       var self=this
-      function helperSort(nextDepth){
-        return Object.keys(nextDepth)
+      if(this.schema.properties){ 
+        return Object.keys(this.schema.properties)
         .filter(x=>Object.keys(self.value).includes(x))
-        .filter(x=>nextDepth.pick ? nextDepth.pick.includes(x) : true) 
+        .filter(x=>this.pick ? this.pick.includes(x) : true) 
         .filter(x=>{
-          return nextDepth.omit ? !nextDepth.omit.includes(x) : true
+          return this.omit ? !this.omit.includes(x) : true
         }) 
         .map(function(x){
-          var out=_.cloneDeep(nextDepth[x])
+          var out=_.cloneDeep(self.schema.properties[x])
           out.name=x
           return out
         })
         .sort((x,y)=>{
           return _.get(x,'propertyOrder',Number.MAX_SAFE_INTEGER)-_.get(y,'propertyOrder',Number.MAX_SAFE_INTEGER)
         })
-      }
-      function recurseDown(currentDepth){
-          if(_.has(currentDepth,"properties")){
-            for(var key in Object.keys(currentDepth.properties)){
-              recurseDown(key)
-            }
-            return helperSort(currentDepth.properties)
-          }
-          if(_.has(currentDepth,"items.properties")){
-            for(var key in Object.keys(currentDepth.items.properties)){
-              recurseDown(key)
-            }
-            return helperSort(currentDepth.properties)
-          }
-        return currentDepth
-      } 
-      if(this.schema.properties){
-        return recurseDown(this.schema)
       }else{
         []
       }
     },
-  //   properties:function(){
-  //     var self=this
-  //     if(this.schema.properties){ 
-  //       return Object.keys(this.schema.properties)
-  //       .filter(x=>Object.keys(self.value).includes(x))
-  //       .filter(x=>this.pick ? this.pick.includes(x) : true) 
-  //       .filter(x=>{
-  //         return this.omit ? !this.omit.includes(x) : true
-  //       }) 
-  //       .map(function(x){
-  //         var out=_.cloneDeep(self.schema.properties[x])
-  //         out.name=x
-  //         return out
-  //       })
-  //       .sort((x,y)=>{
-  //         return _.get(y,'propertyOrder',0)-_.get(x,'propertyOrder',0)
-  //       })
-  //     }else{
-  //       []
-  //     }
-  //   },
     validate:function(){
       var r=this.required ? 'required' : ''
       if(this.schema.maxLength){
