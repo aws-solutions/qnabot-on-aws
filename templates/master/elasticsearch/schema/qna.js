348dd11e64439228e6713ec0de307d2d4a93392e
potterve <potterve@amazon.com>
2018-10-10 06:52:44 -0600

    Changed lambda hook argument max length to 2000 from 140.

--- templates/master/elasticsearch/schema/qna.js
+++ templates/master/elasticsearch/schema/qna.js
@@ -114,7 +114,7 @@ module.exports={
                     items:{
                         title:"Argument",
                         type:"string",
-                        maxLength:140
+                        maxLength:2000
                     }
                 }
             },
