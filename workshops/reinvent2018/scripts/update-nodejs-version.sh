9ac5034052c2455e784931a05fb6a7a9acf6f7aa
bobpskier <potterve@amazon.com>
2018-11-15 07:01:30 -0600

    modified script used to update workshop cloud9 environment update. updated readme for master deployment.

--- workshops/reinvent2018/scripts/update-nodejs-version.sh
+++ workshops/reinvent2018/scripts/update-nodejs-version.sh
@@ -6,16 +6,14 @@ pip --version
 pip install --user aws-sam-cli
 pip install awscli --upgrade --user
 
-nvm install 8.12.0
-nvm alias default 8
-source ~/.nvm/nvm.sh
 
 USER_BASE_PATH=$(python -m site --user-base)
 export PATH=$USER_BASE_PATH/bin:$PATH
 
+nvm install 8.12.0
+nvm alias default 8
 source ~/.nvm/nvm.sh
 
 node --version
 nvm --version
 
-echo $PATH
