6a92eb607996d58c63f65acfc8ad03243c66d940
Bob Strahan <strahanr@amazon.com>
2019-09-17 21:07:29 +0000

    Add mechanism for custom extensions - lambda hooks and UI import

--- templates/examples/outputs.js
+++ templates/examples/outputs.js
@@ -1,25 +1,31 @@
-var fs=require('fs')
-var _=require('lodash')
+var fs=require('fs');
+var _=require('lodash');
 
-
-var js=fs.readdirSync(`${__dirname}/examples/js`)
+var js_example=fs.readdirSync(`${__dirname}/examples/js`)
 .filter(x=>x.match(/(.*).js/))
 .map(file=>{
     var name=file.match(/(.*).js/)[1]
     return `ExampleJSLambda${name}`
 })
-
-var py=fs.readdirSync(`${__dirname}/examples/py`)
+var py_example=fs.readdirSync(`${__dirname}/examples/py`)
 .filter(x=>x.match(/(.*).py/))
 .map(file=>{
     var name=file.match(/(.*).py/)[1]
     return `ExamplePYTHONLambda${name}`
 })
 
-exports.names=py.concat(js)
-exports.outputs=_.fromPairs(exports.names.map(x=>{
-        return [x,{Value:{"Fn::GetAtt":[x,"Arn"]}}]
-    }))
+var js_ext=fs.readdirSync(`${__dirname}/extensions/js_lambda_hooks`)
+.map(name=>{
+    return `EXT${name}`;
+});
+var py_ext=fs.readdirSync(`${__dirname}/extensions/py_lambda_hooks`)
+.map(name=>{
+    return `EXT${name}`;
+});
 
+exports.names=js_example.concat(py_example).concat(js_ext).concat(py_ext) ;
+exports.outputs=_.fromPairs(exports.names.map(x=>{
+        return [x,{Value:{"Fn::GetAtt":[x,"Arn"]}}];
+    }));
 
 
