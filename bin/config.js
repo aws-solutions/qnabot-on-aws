340d6b299b9df374e6eb27970a895803ab3f75cb
Paddy Keane <Paddy.Keane@akqa.com>
2020-01-18 12:43:34 +0000

    + [ADDED] - 'stackNamePrefix' param to config to allow customised StackName in CloudFormation

--- bin/config.js
+++ bin/config.js
@@ -1,11 +1,8 @@
 /*
 Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
 Licensed under the Amazon Software License (the "License"). You may not use this file
 except in compliance with the License. A copy of the License is located at
-
 http://aws.amazon.com/asl/
-
 or in the "license" file accompanying this file. This file is distributed on an "AS IS"
 BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the
 License for the specific language governing permissions and limitations under the License.
@@ -17,7 +14,8 @@ module.exports={
     "publicBucket":"aws-bigdata-blog",
     "publicPrefix":"artifacts/aws-ai-qna-bot",
     "devEmail":"",
-    "namespace":"dev"
+    "namespace":"dev",
+    "stackNamePrefix":"QNA"
 }
 
 if (require.main === module) {
