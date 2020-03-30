93c91794e908758740da62e35bf66b502c09f1ff
Bob Strahan <strahanr@amazon.com>
2020-03-30 19:54:32 +0000

    remove problematic handlebars processing from examples upload

--- templates/examples/examples/cfn.js
+++ templates/examples/examples/cfn.js
@@ -1,7 +1,6 @@
 var response = require('cfn-response')
 var aws=require('aws-sdk')
 var fs=require('fs')
-var handlebars = require('handlebars')
 
 aws.config.region=process.env.AWS_REGION
 var s3=new aws.S3()
@@ -14,10 +13,7 @@ exports.handler = function(event, context,cb) {
             var files=fs.readdirSync(`${__dirname}/examples`)
             Promise.all(files.map(x=>{
                 var name=x
-                var temp_text=fs.readFileSync(`${__dirname}/examples/${x}`,'utf-8')
-                var temp=handlebars.compile(temp_text)
-                var text=temp(event.ResourceProperties)
-
+                var text=fs.readFileSync(`${__dirname}/examples/${x}`,'utf-8')
                 return s3.putObject({
                     Bucket:event.ResourceProperties.Bucket,
                     Key:`examples/documents/${name}`,
