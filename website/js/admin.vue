22a37cdf7da031e50a77374ac59e9a0c746db7cc
Bob Strahan <strahanr@amazon.com>
2020-06-07 14:48:28 +0000

    modify Kibana dashboard URL to launch Dashboards page, rather than named non-existant 'Default' dashboard

--- website/js/admin.vue
+++ website/js/admin.vue
@@ -149,7 +149,7 @@ module.exports={
         subTitle:"Analyze ChatBot usage",
         icon:"show_chart",
         target:'_blank',
-        href:_.get(this,"$store.state.info._links.Kibana.href")+"#/dashboard/Default?_g=()"
+        href:_.get(this,"$store.state.info._links.Kibana.href")+"#/dashboards?_g=()"
       }]
     }
   },
