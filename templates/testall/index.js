3fb0d85cc9213e983b14ed3512b92931a74dd83f
Paddy Keane <Paddy.Keane@akqa.com>
2020-06-09 21:09:35 +0100

    remove .DS_store files being uploaded to S3 artifacts during bootstrapping. (handy for developers using Macs & local development)

--- templates/testall/index.js
+++ templates/testall/index.js
@@ -2,7 +2,7 @@ var fs=require('fs')
 var _=require('lodash')
 
 var files=fs.readdirSync(`${__dirname}`)
-    .filter(x=>!x.match(/README.md|Makefile|index|test|outputs/))
+    .filter(x=>!x.match(/README.md|Makefile|index|test|outputs|.DS_Store/))
     .map(x=>require(`./${x}`))
 
 module.exports={
