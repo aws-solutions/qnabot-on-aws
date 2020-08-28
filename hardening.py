c277e153aed7a77c05a98b556d14c78893407ce7
Karl Thomas <thokrl+github@amazon.com>
2020-08-28 13:44:01 -0400

    Added key policies

--- hardening.py
+++ hardening.py
@@ -5,18 +5,20 @@ import base64
 import sys
 
 
-
 parser = argparse.ArgumentParser(description='Uses a specified CMK to encrypt QnABot Lambdas and Parameter Store settings')
 parser.add_argument("stack_arn", help="the arn of the QnABot CloudFormation Stack")
 parser.add_argument("cmk_arn", help="the ARN of the Customer Master Key to use for encryption")
 
 lambda_client = boto3.client('lambda')
 iam_client = boto3.client('iam')
+role_paginator = iam_client.get_paginator('list_role_policies')
 kms_client = boto3.client("kms")
 cloudformation_client = boto3.client('cloudformation')
 ssm_client = boto3.client('ssm')
 s3_client = boto3.client('s3')
 ddb_client = boto3.client('dynamodb')
+sts_client = boto3.client('sts')
+
 
 
 args = type('', (), {})()
@@ -25,20 +27,98 @@ if len(sys.argv) != 1:
     args = parser.parse_args()
 else:
     args.stack_arn = "QnaBotVPC4"
-    args.cmk_arn = "arn:aws:kms:us-east-1:123456778:key/aaaaa-bbbb-cccc-dddd-eeeee"
+    args.cmk_arn = "arn:aws:kms:us-east-1:1234567890:key/aaaaaaa-bbbb-cccc-dddd-123456789"
 
+policy_name = "CMKPolicy4"
 policy_document = {
     "Version":"2012-10-17",
     "Statement":[
         {
             "Effect":"Allow",
-            "Action":"kms:Decrypt",
+            "Action":[
+                "kms:Decrypt",
+                "kms:Encrypt",
+                "kms:GenerateDataKey"
+            ],
             
             "Resource":args.cmk_arn
         }
     ]
 }
 
+
+
+cmk_roles_logical_ids = [
+    'S3AccessRole',
+    'FirehoseESS3Role',
+    'AdminRole'
+]
+
+cmk_roles_physical_ids = []
+
+def assign_role(role_name):
+    role_iterator = role_paginator.paginate(
+        RoleName=role_name,
+        PaginationConfig={
+            'MaxItems': 1000,
+            'PageSize': 1000
+        }
+    )
+
+    print(f"Updating role {role_name}...")
+    cmk_policy_exists = False
+    for role in role_iterator:
+        if policy_name in role["PolicyNames"]:
+            cmk_policy_exists = True
+            break
+
+    if not cmk_policy_exists:
+        iam_client.put_role_policy(RoleName=role_name, PolicyName = policy_name,PolicyDocument=json.dumps(policy_document))
+
+def put_key_policy (stackname,roles):
+    response = kms_client.get_key_policy(KeyId = args.cmk_arn, PolicyName='default')
+    policy = response['Policy'].replace("\n","")
+    policy = json.loads(policy)
+    
+    caller_identity = sts_client.get_caller_identity()
+
+    new_statement = []
+    for  statement in policy["Statement"]:
+        if(statement["Sid"] != stackname):
+            new_statement.append(statement)
+    policy["Statement"] = new_statement
+
+    formatted_roles = []
+    for role in roles:
+        formatted_roles.append(f"arn:aws:iam::{caller_identity['Account']}:role/{role}")
+
+    policy["Statement"].append(
+
+            {
+                "Sid": stackname,
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": formatted_roles
+                },
+                "Action": [
+                    "kms:Encrypt",
+                    "kms:Decrypt",
+                    "kms:GenerateDataKey"
+                ],
+                "Resource": "QNA*"
+            }
+        )
+
+    print(f"Updating policy for key  {args.cmk_arn} with statement {json.dumps(policy)}")
+
+
+    kms_client.put_key_policy(
+        KeyId = args.cmk_arn,
+        PolicyName = "default",
+        Policy = json.dumps(policy)
+    )
+
+    print(f"Policy for key {args.cmk_arn} updated with statement {policy}")
 
 def process_stacks(stackname):
     paginator = cloudformation_client.get_paginator('list_stack_resources')
@@ -48,8 +128,7 @@ def process_stacks(stackname):
             'MaxItems': 10000#,
         }
     )
-
-    role_paginator = iam_client.get_paginator('list_role_policies')
+    
 
     for response in response_iterator:
         lambda_resources = filter(lambda x: x["ResourceType"] == "AWS::Lambda::Function",response["StackResourceSummaries"])
@@ -57,27 +136,13 @@ def process_stacks(stackname):
 
         for lambda_func in lambda_resources:
             lambda_client.update_function_configuration(FunctionName=lambda_func["PhysicalResourceId"],KMSKeyArn=args.cmk_arn)
-            print(f"Updated function {lambda_func} in stack {stackname}")
+            print(f"Updated function {lambda_func['PhysicalResourceId']} in stack {stackname}")
             
             lambda_configuration = lambda_client.get_function_configuration(FunctionName=lambda_func["PhysicalResourceId"])
             role_name = lambda_configuration["Role"].split("/")[-1]
 
-            role_iterator = role_paginator.paginate(
-                RoleName=role_name,
-                PaginationConfig={
-                    'MaxItems': 1000,
-                    'PageSize': 1000
-                }
-            )
+            assign_role(role_name)
 
-            cmk_policy_exists = False
-            for role in role_iterator:
-                if "CMKPolicy" in role["PolicyNames"]:
-                    cmk_policy_exists = True
-                    break
-
-            if not cmk_policy_exists:
-                iam_client.put_role_policy(RoleName=role_name, PolicyName = "CMKPolicy",PolicyDocument=json.dumps(policy_document))
 
         ssm_parameters = filter(lambda x: x["ResourceType"] == "AWS::SSM::Parameter",response["StackResourceSummaries"])
 
@@ -119,17 +184,27 @@ def process_stacks(stackname):
 
         ddb_tables = filter(lambda x: x["ResourceType"] == "AWS::DynamoDB::Table",response["StackResourceSummaries"])
         for table in ddb_tables:
-            ddb_client.update_table(
-                TableName = table["PhysicalResourceId"],
-                SSESpecification ={
-                    'Enabled': True,
-                    'SSEType': 'KMS',
-                    'KMSMasterKeyId': args.cmk_arn
-                }
-            )
+            table_description = ddb_client.describe_table(TableName = table["PhysicalResourceId"])
+
+            if('KMSMasterKeyArn' not in table_description["Table"]['SSEDescription'] or  table_description["Table"]['SSEDescription']['KMSMasterKeyArn']!= args.cmk_arn ):
+                ddb_client.update_table(
+                    TableName = table["PhysicalResourceId"],
+                    SSESpecification ={
+                        'Enabled': True,
+                        'SSEType': 'KMS',
+                        'KMSMasterKeyId': args.cmk_arn
+                    }
+                )
+
+        role_resources = filter(lambda x: x["LogicalResourceId"] in cmk_roles_logical_ids, response["StackResourceSummaries"])
+        for role_resource in role_resources:
+            cmk_roles_physical_ids.append(role_resource["PhysicalResourceId"])
+            assign_role(role_resource["PhysicalResourceId"])
+
 
 
 process_stacks(args.stack_arn)
+
 
 paginator = cloudformation_client.get_paginator('list_stack_resources')
 response_iterator = paginator.paginate(
@@ -139,11 +214,14 @@ response_iterator = paginator.paginate(
     }
 )
 
+
 for response in response_iterator:
     stacks = filter(lambda x: x["ResourceType"] == "AWS::CloudFormation::Stack",response["StackResourceSummaries"])
     for stack in stacks:
-        print(f"Processing stack {stack}")
+        print(f"Processing stack {stack['PhysicalResourceId']}")
         process_stacks(stack["PhysicalResourceId"])
+
+put_key_policy(args.stack_arn,cmk_roles_physical_ids)
 
 
 
