b1ed90627ecec6c639c411c45af6d98c063c6a61
Paddy Keane <Paddy.Keane@akqa.com>
2020-09-03 21:29:41 +0100

    Adding an 'Alexa Reprompt' to the qna schema

--- lambda/schema/qna.js
+++ lambda/schema/qna.js
@@ -48,11 +48,18 @@ module.exports={
             },
             propertyOrder: 3
         },
+        rp:{
+            type:"string",
+            title:"Alexa Reprompt",
+            description:"Enter the Alexa reprompt to returned if the user does not respond. (SSML autodetection)",
+            maxLength:8000,
+            propertyOrder: 4
+        },
         t:{
             type:"string",
             description:"Assign a topic to this item, to support follow up questions on the same topic. (Sets session attribute 'topic' in response)",
             title:"Topic",
-            propertyOrder: 4
+            propertyOrder: 5
         },
         r:{
             title:"Response card",
@@ -103,14 +110,14 @@ module.exports={
                     propertyOrder: 3
                 }
             },
-            propertyOrder:5,
+            propertyOrder:6,
             required:["title"]
         },
         l:{
             type:"string",
             description:"Enter your lambda function name/ARN to dynamically create or modify answers, or to redirect to a different question.",
             title:"Lambda Hook",
-            propertyOrder:6
+            propertyOrder:7
         },
         args:{
             title:"Lambda Hook Arguments",
@@ -121,13 +128,13 @@ module.exports={
                 type:"string",
                 maxLength:2000
             },
-            propertyOrder:7
+            propertyOrder:8
         },
         elicitResponse:{
             title:"Elicit Response",
             description:"If your answer includes a question to the user, configure QnABot to process and capture the user's response as session attributes.",
             type:"object",
-            propertyOrder:8,
+            propertyOrder:9,
             properties:{
                 responsebot_hook:{
                     title:"Elicit Response: ResponseBot Hook",
@@ -150,17 +157,16 @@ module.exports={
             description:"Automatically move on to another item based on the question string returned by this rule. Rule can be a single-quoted string, e.g. 'next question', or a JavaScript conditional expression that evaluates to a string, e.g. (SessionAttributes.namespace.Yes_No == \"Yes\" ) ? \"Yes question\" : \"No Question\", or a Lambda Function Name or ARN that returns a string specified as \"Lambda::FunctionName\". Function name must start with \"QNA\".",
             type:"string",
             maxLength:4000,
-            propertyOrder:9
+            propertyOrder:10
         },
         next:{
             title:"Guided Navigation: Next QID",
             description:"If applicable, enter the QID of the document(s) that is/are next in the sequence, otherwise leave blank. Be careful; if you set this field to an earlier document in the sequence, you might make your sequence loop forever, which would not be fun!  You can add more QIDs after the first, but they won't do anything at the moment.",
             type:"string",
             maxLength:100,
-            propertyOrder:10
+            propertyOrder:11
         },
     },
     required:["qid","q","a"]
 
 }
-                
