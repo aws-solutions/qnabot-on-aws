8e746ad728a4f6920e4964624573e1bfbbe4bd52
Karl Thomas <thokrl+github@amazon.com>
2020-11-01 12:34:11 -0500

    Kendra FAQ Fix -- initial commit

--- lambda/export/parseJSON.js
+++ lambda/export/parseJSON.js
@@ -41,6 +41,82 @@ async function qnaJsonParser(params) {
   
 }
 
+async function qnaJsontoKendraJsonParser(params){
+  // const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  // const csvWriter = createCsvWriter({
+  //   path: params.output_path,
+  //   header: [
+  //     {id: 'question', title: 'Question'},
+  //     {id: 'answer', title: 'Answer'},
+  //     {id: 'link', title: 'Document URL (optional)'},
+  //   ]
+  // });
+
+  var data = {
+    "SchemaVersion": 1,
+    "FaqDocuments": [
+        // {
+        //     "Question": "How tall is the Space Needle?",
+        //     "Answer": "605 feet"
+        // },
+        // {
+        //     "Question": "How tall is Smith Tower?",
+        //     "Answer": "484 feet",
+        //     "Attributes": {
+        //         "_source_uri": "https://www.smithtower.com",
+        //         "_authors": ["Richard Roe", "Jorge Souza"],
+        //         "_category": "Buildings",
+        //         "_created_at": "2020-09-15T22:40:46Z",
+        //         "_last_updated_at": "2020-09-15T22:40:46-08:00",
+        //         "_version": "v1",
+        //         "_view_count": 123
+        //     },
+        //     "AccessControlList": [
+        //        {
+        //            "Name": "user@amazon.com",
+        //            "Type": "USER",
+        //            "Access": "ALLOW"
+        //        },
+        //        {
+        //            "Name": "Admin",
+        //            "Type": "GROUP",
+        //            "Access": "ALLOW"
+        //        }
+            ]
+        }
+    
+}
+  
+  var qna = `{"qna":[${params.content.toString().replace(/\n/g,',\n')}]}`
+  params.content = JSON.parse(qna);
+  const q_list = params.content.qna;
+  
+  //const data = [];
+  q_list.forEach(function(elem) {
+      var json_doc = JSON.stringify(elem);    // puts entire JSON structure in URL field
+      if (elem.q) {                           // qna type questions (standard)
+        elem.q.forEach(function(ques){
+          var entry = {
+            Question:ques, 
+            Answer:elem.a, 
+            Attributes:{
+              link: json_doc
+            }
+          };
+          data.FaqDocuments.push(entry);
+        })
+      } else {
+        console.log(`this element is not supported with KendraFAQ and was skipped in the sync: ${JSON.stringify(elem)}`);
+      }
+      
+  });
+  
+  return data;
+  
+
+
+
 exports.handler = async (params) => {
-    return await qnaJsonParser(params);
+    //return await qnaJsonParser(params);
+    return await qnaJsontoKendraJsonParser(params)
 };
