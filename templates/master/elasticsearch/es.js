4ed47633293dca7b46f4d2c36f2c98507b8e1fc5
Bob Potterveld <potterve@amazon.com>
2021-02-25 12:47:03 -0700

    Change both encrypted and unencrypted installations to use t3.medium.elasticsearch. Changed default elasticsearch node count to be 4 instead of 2.

--- templates/master/elasticsearch/es.js
+++ templates/master/elasticsearch/es.js
@@ -3,7 +3,7 @@ var properties={
     "ElasticsearchClusterConfig": {
        "DedicatedMasterEnabled": false,
        "InstanceCount": {"Ref":"ElasticSearchNodeCount"},
-       "InstanceType": {"Fn::If": [ "Encrypted", "t3.medium.elasticsearch", "t3.medium.elasticsearch"]},
+       "InstanceType": {"Fn::If": [ "Encrypted", "c5.large.elasticsearch", "t3.medium.elasticsearch"]},
        "ZoneAwarenessEnabled": "true"
     },
     "EBSOptions": {
