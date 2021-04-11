b03eab42e0e01a02084e180d04771d16d7e023a8
Bob Strahan <strahanr@amazon.com>
2021-04-11 20:25:24 +0000

    Set ALT_SEARCH_KENDRA_FALLBACK_CONFIDENCE_SCORE to default to HIGH.. if threshold for fallback is lower than threshold for FAQ, chances are that FAQ answer will be returned via Fallback

--- templates/master/default-settings.js
+++ templates/master/default-settings.js
@@ -13,7 +13,7 @@ var default_settings = {
     ENABLE_MULTI_LANGUAGE_SUPPORT: "false", //User can override and set to true to Enable Multilanguage support
     ENABLE_CUSTOM_TERMINOLOGY: "false",
     MINIMUM_CONFIDENCE_SCORE: 0.6, //User can override this value to set the minimum confidence they accept using CustomQnABotSettings
-    ALT_SEARCH_KENDRA_FALLBACK_CONFIDENCE_SCORE: "MEDIUM", //Should be one of 'VERY_HIGH'|'HIGH'|'MEDIUM'|'LOW'
+    ALT_SEARCH_KENDRA_FALLBACK_CONFIDENCE_SCORE: "HIGH", //Should be one of 'VERY_HIGH'|'HIGH'|'MEDIUM'|'LOW'
     ALT_SEARCH_KENDRA_FAQ_CONFIDENCE_SCORE: "HIGH", //Should be one of 'VERY_HIGH'|'HIGH'|'MEDIUM'|'LOW'
     ALT_SEARCH_KENDRA_INDEXES: "${DefaultKendraIndexId}", // Add Kendra index to array to enable Amazon Kendra as a fallback source of answers
     ALT_SEARCH_KENDRA_S3_SIGNED_URLS: "true", // If S3 document URL is in the search result, convert to signed URL. Make sure IAM ExtensionLambdaRole has access to S3 objects in Kendra index (default role grants access to buckets starting with name QNA or qna).
