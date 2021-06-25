8ae1d5403977763e139dd18edc8c4cd11fcc8559
Bob Strahan <strahanr@amazon.com>
2021-06-25 20:15:31 +0000

    update to use public SAR package for Cfn Custom Resource

--- templates/master/lexv2-cfn-customresource.js
+++ templates/master/lexv2-cfn-customresource.js
@@ -3,11 +3,11 @@ module.exports={
       "Type": "AWS::Serverless::Application",
       "Properties": {
          "Location": {
-            "ApplicationId": "arn:aws:serverlessrepo:us-east-1:506886316466:applications/lex-v2-cfn-cr",
+            "ApplicationId": "arn:aws:serverlessrepo:us-east-1:777566285978:applications/lex-v2-cfn-cr",
             "SemanticVersion": "0.1.0"
          },
          "Parameters": {
-            "LogLevel": "INFO"
+            "LogLevel": "DEBUG"
          }
       }
    }
