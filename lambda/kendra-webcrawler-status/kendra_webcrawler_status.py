b24abaeccdc6d2655767b08addd518825c123c16
Trinity Key <hellotrinitykey@gmail.com>
2021-08-12 11:53:37 -0700

    resolving flake8 errors

--- lambda/kendra-webcrawler-status/kendra_webcrawler_status.py
+++ lambda/kendra-webcrawler-status/kendra_webcrawler_status.py
@@ -1,17 +1,12 @@
-import boto3
 import os
 import json
+import boto3
 import functools
 
-# var pendingStatus = [
-#       "SYNCING",
-#       "INCOMPLETE",
-#       "STOPPING",
-#       "SYNCING_INDEXING",
-#     ];
 
 client = boto3.client('kendra')
 ssm = boto3.client('ssm')
+
 
 def handler(event, context):
     Name = os.environ.get('DATASOURCE_NAME')
@@ -19,7 +14,7 @@ def handler(event, context):
     IndexId = settings['KENDRA_WEB_PAGE_INDEX']
     data_source_id = get_data_source_id(IndexId, Name)
 
-    if data_source_id == None:
+    if data_source_id is None:
         return {"Status": 'NOTINDEXED'}
     else:
         return kendra_list_data_source_sync_jobs(IndexId, data_source_id)
@@ -31,7 +26,6 @@ def get_settings():
 
     default_settings = ssm.get_parameter(Name=default_settings_key, WithDecryption=True)
     default_settings = json.loads(default_settings['Parameter']['Value'])
-    
     custom_settings = ssm.get_parameter(Name=custom_settings_key, WithDecryption=True)
     custom_settings = json.loads(custom_settings['Parameter']['Value'])
     default_settings.update(custom_settings)
@@ -41,8 +35,8 @@ def get_settings():
 
 def get_data_source_id(index_id, data_source_name):
     response = client.list_data_sources(
-    IndexId=index_id,
-    MaxResults=5
+        IndexId=index_id,
+        MaxResults=5
     )
 
     # filtered = filter(lambda item: item['Name'] == data_source_name, response['SummaryItems'])
@@ -51,41 +45,38 @@ def get_data_source_id(index_id, data_source_name):
             return item['Id']
     return None
 
+
 def kendra_list_data_source_sync_jobs(IndexId, data_source_id):
     response = client.list_data_source_sync_jobs(
-        Id = data_source_id,
-        IndexId= IndexId,
+        Id=data_source_id,
+        IndexId=IndexId,
     )
 
     # get current status by sorting the result by start time descending order
     # if status is equal to status in var status list, the status will be pending
     pending_statuses = [
-      "SYNCING",
-      "STOPPING",
-      "SYNCING_INDEXING",
+        "SYNCING",
+        "STOPPING",
+        "SYNCING_INDEXING",
     ]
 
     latest_history_item = functools.reduce(lambda x, y: x if x['StartTime'] > y['StartTime'] else y, response['History'])
     status = "PENDING" if latest_history_item['Status'] in pending_statuses else "SUCCEEDED"
     print(status)
-    
-    result = list(map(lambda item: {'StartTime': item['StartTime'].strftime("%m/%d/%Y, %H:%M:%S"), 
-                                'EndTime': item['EndTime'].strftime("%m/%d/%Y, %H:%M:%S") if 'EndTime' in item else '',
-                                'Status': item['Status'],
-                                'ErrorMessage': item['ErrorMessage'] if 'ErrorMessage' in item else '',
-                                'Metrics': item['Metrics']
-                                }, response['History']))
+    result = list(map(lambda item: {'StartTime': item['StartTime'].strftime("%m/%d/%Y, %H:%M:%S"),
+                                    'EndTime': item['EndTime'].strftime("%m/%d/%Y, %H:%M:%S") if 'EndTime' in item else '',
+                                    'Status': item['Status'],
+                                    'ErrorMessage': item['ErrorMessage'] if 'ErrorMessage' in item else '',
+                                    'Metrics': item['Metrics']
+                                    }, response['History']))
     return {"Status": status, "History": result}
     # return status, list of values in result
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
 
     os.environ["CUSTOM_SETTINGS_PARAM"] = 'CFN-CustomQnABotSettings-4VLlLa3BAFAB'
     os.environ["DEFAULT_SETTINGS_PARAM"] = 'CFN-DefaultQnABotSettings-J3x2iwsNoWkx'
     os.environ["DATASOURCE_NAME"] = 'QNABotKendraNativeCrawler-J3x2iwsNoWkx-v2'
 
-
-    handler(None, None);
-
-
+    handler(None, None)
--- lambda/kendra-webcrawler/kendra_webcrawler.py
+++ lambda/kendra-webcrawler/kendra_webcrawler.py
@@ -1,29 +1,31 @@
-import boto3
 import os
 import json
+import boto3
+
 
 client = boto3.client('kendra')
 ssm = boto3.client('ssm')
 
+
 def handler(event, context):
     Name = os.environ.get('DATASOURCE_NAME')
-    Type = 'WEBCRAWLER'
     RoleArn = os.environ.get('ROLE_ARN')
+    Type = 'WEBCRAWLER'
     Description = 'QnABot WebCrawler Index'
 
     settings = get_settings()
     IndexId = settings['KENDRA_WEB_PAGE_INDEX']
-    URLs = settings['KENDRA_INDEXER_URLS'].replace(' ','').split(',')
+    URLs = settings['KENDRA_INDEXER_URLS'].replace(' ', '').split(',')
     data_source_id = get_data_source_id(IndexId, Name)
 
-    if data_source_id == None:
-        data_source_id = kendra_create_data_source(client, IndexId, Name, Type, RoleArn, Description, URLs);
-        kendra_sync_data_source(IndexId, data_source_id);
+    if data_source_id is None:
+        data_source_id = kendra_create_data_source(client, IndexId, Name, Type, RoleArn, Description, URLs)
+        kendra_sync_data_source(IndexId, data_source_id)
     else:
-        kendra_update_data_source(IndexId, data_source_id, URLs);
-        kendra_sync_data_source(IndexId, data_source_id);
-        
+        kendra_update_data_source(IndexId, data_source_id, URLs)
+        kendra_sync_data_source(IndexId, data_source_id)
     return {"IndexId": IndexId, "DataSourceId": data_source_id}
+
 
 def get_settings():
     default_settings_key = os.environ.get("DEFAULT_SETTINGS_PARAM")
@@ -31,7 +33,6 @@ def get_settings():
 
     default_settings = ssm.get_parameter(Name=default_settings_key, WithDecryption=True)
     default_settings = json.loads(default_settings['Parameter']['Value'])
-    
     custom_settings = ssm.get_parameter(Name=custom_settings_key, WithDecryption=True)
     custom_settings = json.loads(custom_settings['Parameter']['Value'])
     default_settings.update(custom_settings)
@@ -42,8 +43,8 @@ def get_settings():
 
 def get_data_source_id(index_id, data_source_name):
     response = client.list_data_sources(
-    IndexId=index_id,
-    MaxResults=5
+        IndexId=index_id,
+        MaxResults=5
     )
 
     # filtered = filter(lambda item: item['Name'] == data_source_name, response['SummaryItems'])
@@ -52,70 +53,72 @@ def get_data_source_id(index_id, data_source_name):
             return item['Id']
     return None
 
+
 def kendra_create_data_source(client, IndexId, Name, Type, RoleArn, Description, URLs):
     response = client.create_data_source(
-    Name=Name,
-    IndexId=IndexId,
-    Type=Type,
-    RoleArn=RoleArn,
-    Description=Description,
-    Configuration={
-        'WebCrawlerConfiguration': {
-            'Urls': {
-                'SeedUrlConfiguration': {
-                    'SeedUrls': URLs,
-                    'WebCrawlerMode': 'EVERYTHING'
-                }
-            },
-            'CrawlDepth': 2
+        Name=Name,
+        IndexId=IndexId,
+        Type=Type,
+        RoleArn=RoleArn,
+        Description=Description,
+        Configuration={
+            'WebCrawlerConfiguration': {
+                'Urls': {
+                    'SeedUrlConfiguration': {
+                        'SeedUrls': URLs,
+                        'WebCrawlerMode': 'EVERYTHING'
+                    }
+                },
+                'CrawlDepth': 2
+            }
         }
-    }
     )
     print(json.dumps(response))
     return response['Id']
 
+
 def kendra_sync_data_source(IndexId, data_source_id):
     response = client.start_data_source_sync_job(
-    Id=data_source_id,
-    IndexId=IndexId
+        Id=data_source_id,
+        IndexId=IndexId
     )
     print(json.dumps(response))
     return response
+
 
 def kendra_update_data_source(IndexId, data_source_id, URLs):
     response = client.update_data_source(
-    Id=data_source_id,
-    IndexId=IndexId,
-    Configuration={
-        'WebCrawlerConfiguration': {
-            'Urls': {
-                'SeedUrlConfiguration': {
-                    'SeedUrls': URLs,
-                    'WebCrawlerMode': 'EVERYTHING'
-                }
-            },
-            'CrawlDepth': 2
+        Id=data_source_id,
+        IndexId=IndexId,
+        Configuration={
+            'WebCrawlerConfiguration': {
+                'Urls': {
+                    'SeedUrlConfiguration': {
+                        'SeedUrls': URLs,
+                        'WebCrawlerMode': 'EVERYTHING'
+                    }
+                },
+                'CrawlDepth': 2
+            }
         }
-    }
     )
     print(json.dumps(response))
     return response
 
+
 def kendra_list_data_source_sync_jobs(IndexId, data_source_id):
     response = client.list_data_source_sync_jobs(
-        Id = data_source_id,
-        IndexId= IndexId,
+        Id=data_source_id,
+        IndexId=IndexId,
     )
 
-    result = list(map(lambda item: {'StartTime': item['StartTime'], 
-                                'EndTime': item['EndTime'],
-                                'Status': item['Status'],
-                                'ErrorMessage': item['ErrorMessage'],
-                                'Metrics': item['Metrics']
-                                }, response['History']))
+    result = list(map(lambda item: {'StartTime': item['StartTime'],
+                                    'EndTime': item['EndTime'],
+                                    'Status': item['Status'],
+                                    'ErrorMessage': item['ErrorMessage'],
+                                    'Metrics': item['Metrics']
+                                    }, response['History']))
 
 
-if __name__ == "__main__":    
-
-    handler(event, None);
-    
+if __name__ == "__main__":
+    handler(event, None)
