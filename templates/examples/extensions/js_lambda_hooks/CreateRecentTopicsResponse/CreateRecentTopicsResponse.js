bc934d234c1b56f8442492439d43561e321f8feb
Karl Thomas <thokrl+github@amazon.com>
2021-10-08 13:57:22 -0400

    Refactored the CreateRecetTopicsResponse example extension to use the Javascript SDK

--- templates/examples/extensions/js_lambda_hooks/CreateRecentTopicsResponse/CreateRecentTopicsResponse.js
+++ templates/examples/extensions/js_lambda_hooks/CreateRecentTopicsResponse/CreateRecentTopicsResponse.js
@@ -1,27 +1,10 @@
 const _ = require("lodash");
-exports.handler = async function(event, context) {
-  //
-  console.log(event);
-  //Retrieve the args passed in via the Content Designer
-  var args = _.get(event, "res.result.args");
-  var start = 0;
-  var end = 3;
-  if (args) {
-    args = JSON.parse(args);
-    start = args.start != undefined ? args.start : start;
-    end = args.end != undefined ? args.end : end;
-  }
-  
-  var existingButtons = _.get(event, "res.card.buttons", [])
-  //Initialize the response card object in the response
-  _.set(event, "res.card", {
-    title: "Recent Topics",
-    send: true,
-    buttons: [],
-  });
+const hook = require("lambda_hook_sdk/hooks")
 
-  //Retrieve the settings from the request object
-  var settings = _.get(event, "req._settings", {});
+
+function create_buttons(event,start=0,stop=10) {
+  let buttons = []
+  var settings = hook.list_settings(event);
   var topicMap = {},
     topicKey;
   for (var key of Object.keys(settings)) {
@@ -31,18 +14,13 @@ exports.handler = async function(event, context) {
       topicMap[topicKey] = settings[key];
     }
   }
-
-  //Retrieve the "recent topics" from the userInfo object.  
-  //All properties stored in the DynamoDB table for a user will be part
-  //of the res._userInfo object
-  var userTopics = event.res._userInfo.recentTopics.sort((t1, t2) => {
+  var userTopics = hook.get_user_attribute(event, "recentTopics", []).sort((t1, t2) => {
     if (t1.dateTime == t2.dateTime) {
       return 0;
     }
     return t2.dateTime < t1.dateTime ? -1 : 1;
   });
-
-  for (var userTopic of userTopics.slice(start, end)) {
+  for (var userTopic of userTopics.slice(start, stop)) {
     if (!(userTopic.topic in topicMap)) {
       continue;
     }
@@ -51,18 +29,56 @@ exports.handler = async function(event, context) {
     if (!description || !qid) {
       console.log(
         "WARNING: The topic mapping topic::" +
-          userTopic.topic +
-          " is not defined properly.  The format should be <description>::<QID>. Using the description as the value."
+        userTopic.topic +
+        " is not defined properly.  The format should be <description>::<QID>. Using the description as the value."
       );
+      continue;
     }
-
-    //Add the buttons to the response object
-    event.res.card.buttons.push({
-      text: description,
-      value: "qid::" + qid,
-    });
-
+    buttons.push({
+      event: event,
+      description: description,
+      qid: qid
+    })
   }
-  event.res.card.buttons = event.res.card.buttons.concat(existingButtons)
-  return event;
+  return buttons
+}
+
+exports.handler = async function (event, context) {
+  let step = hook.get_step(event)
+
+  console.log(event)
+  if (step == "postprocess") {
+    let buttons = create_buttons(event)
+    if (buttons.length == 0) {
+      let recentTopicButton = hook.get_setting(event, "RECENT_TOPICS_BUTTON_VALUE")
+      if (recentTopicButton) {
+        let buttons = hook.list_response_card_buttons(event)
+        let filteredButtons = buttons.filter(r => r.value != recentTopicButton)
+        event.res.card.buttons = filteredButtons
+        event.res.result.r.buttons = filteredButtons
+      }
+    }
+    console.log(JSON.stringify(event))
+
+    return event
+  }
+
+  if (step == "preprocess") {
+    return event
+  }
+  //Retrieve the args passed in via the Content Designer
+  var args = hook.get_args(event)
+  var start = 0;
+  var end = 3;
+  if (args) {
+    start = args.start != undefined ? args.start : start;
+    end = args.end != undefined ? args.end : end;
+  }
+
+  hook.set_response_card_title("Recent Topics", false)
+
+  let buttons = create_buttons(event,start,end)
+  buttons.forEach(index => hook.add_response_card_button(index.event, index.description, index.qid, true, true))
+
+  return hook.validate_response(event);
 };
