b402a0cac7ab6d794cac3009da6da5f7cd87b031
Suraj Tripathi <acc.surajtripthi@gmail.com>
2022-09-01 09:35:20 +0530

    handling message length for lambda hooks case

--- lambda/fulfillment/lib/middleware/lex.js
+++ lambda/fulfillment/lib/middleware/lex.js
@@ -473,7 +473,7 @@ function assembleLexV2Response(request, response) {
               if(imgUrlLength > 250){
                   qnabot.log("ResponseCard Image URL length is greater than the max limit (250 chars). Client is LexWebUI. Sending ResponseCard as session attribute rather than as Lex ImageresponseCard to avoid hitting the Lex URL length limit.")
               } else { 
-                  out.messages[1] = {
+                  out.messages[out.messages.length] = {
                       "contentType": "ImageResponseCard",
                       "imageResponseCard": imageResponseCardV2            
                     };
