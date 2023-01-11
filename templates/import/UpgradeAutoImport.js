4360aeb746f9b882ed8d82e350f8d0d6dcf399a4
Bob Strahan <strahanr@amazon.com>
2023-01-11 19:06:09 +0000

    back up and restore content during update if embedding dimension is changed

--- templates/import/UpgradeAutoImport.js
+++ templates/import/UpgradeAutoImport.js
@@ -18,6 +18,7 @@ module.exports={
             index:{"Ref":"VarIndex"},
             encryption:{"Ref":"Encryption"},
             es_endpoint:{"Ref":"EsEndpoint"},
+            addl_import_triggers:{"Fn::Sub":"${EmbeddingsDimensions}"}
         }
     },
     "PostUpgradeImportMetrics":{
