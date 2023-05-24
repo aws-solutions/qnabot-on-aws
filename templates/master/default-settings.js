ff7f6851b139184d8f14a31db1c36cb9e7940f22
Bob Strahan <strahanr@amazon.com>
2023-05-24 14:25:47 +0000

    Detect no_hits just from phrase 'Sorry,'

--- templates/master/default-settings.js
+++ templates/master/default-settings.js
@@ -93,7 +93,7 @@ var default_settings = {
     LLM_QA_SHOW_CONTEXT_TEXT: "TRUE",
     LLM_QA_SHOW_SOURCE_LINKS: "TRUE",
     LLM_CHAT_HISTORY_MAX_MESSAGES: 12,
-    LLM_QA_NO_HITS_REGEX: 'Sorry, I don\'t know //remove comment to enable custom no match (no_hits) when LLM does not know the answer.',
+    LLM_QA_NO_HITS_REGEX: 'Sorry,  //remove comment to enable custom no match (no_hits) when LLM does not know the answer.',
 };
 const defaultModelParams = `{\\"temperature\\":0}`;
 const anthropicModelParams = `{\\"temperature\\":0, \\"modelName\\":\\"claude-instant-v1-100k\\"}`;
