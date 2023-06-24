93d7579092b836b965baee3d565f5bf2b597bdf1
Bob Strahan <strahanr@amazon.com>
2023-06-24 22:21:42 +0000

    make defaultGenerateQueryPromptTemplate more consistent

--- templates/master/default-settings.js
+++ templates/master/default-settings.js
@@ -96,7 +96,7 @@ var default_settings = {
     LLM_CHAT_HISTORY_MAX_MESSAGES: 12,
     LLM_QA_NO_HITS_REGEX: 'Sorry,  //remove comment to enable custom no match (no_hits) when LLM does not know the answer.',
 };
-const defaultGenerateQueryPromptTemplate = 'Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.<br>Chat History: <br>{history}<br>Follow Up Input: {input}<br>Standalone question:';
+const defaultGenerateQueryPromptTemplate = 'Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.<br>Chat History: <br>{history}<br>Follow up question: {input}<br>Standalone question:';
 const defaultQAPromptTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Write the answer in up to 5 complete sentences.<br><br>{context}<br><br>Question: {query}<br>Helpful Answer:`;
 const defaultModelParams = `{\\"temperature\\":0.01, \\"return_full_text\\":false, \\"max_new_tokens\\": 150}`;
 const anthropicGenerateQueryPromptTemplate = `<br><br>Human: Here is a chat history in <chatHistory> tags:<br><chatHistory><br>{history}<br></chatHistory><br>Human: And here is a follow up question or statement from the human in <followUpMessage> tags:<br><followUpMessage><br>{input}<br></followUpMessage><br>Human: Rephrase the follow up question or statement as a standalone question or statement that makes sense without reading the chat history.<br><br>Assistant: Here is the rephrased follow up question or statement:`;
