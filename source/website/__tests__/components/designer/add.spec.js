3e779628bca3eb234282fa9925584a4702e971d3
Michael Lin <mikemlin@amazon.com>
2025-02-23 20:49:49 -0800

    Update to version v7.0.1

--- source/website/__tests__/components/designer/add.spec.js
+++ source/website/__tests__/components/designer/add.spec.js
@@ -80,4 +80,83 @@ describe('add vue', () => {
         await wrapper.vm.add();
         expect(store.dispatch).toHaveBeenCalledWith('api/check', '1');
     });
+
+    test('add -- markdown sanitized as expected', async () => {
+        store.dispatch.mockReturnValueOnce(Promise.resolve(false));
+        const wrapper = mount(add, {
+            global: {
+                mocks: {
+                    $store: store,
+                },
+            },
+        });
+
+        wrapper.vm.$data.data = {
+            qna: {
+                qid:"test",
+                q:["test"],
+                a:"test",
+                alt:{
+                    markdown:"<p style='white-space'>Testing Markdown Sanitization</p><test></test>"
+                },
+                type:"qna"
+            }
+        };
+
+
+
+        await wrapper.vm.add();
+
+        wrapper.vm.$data.data.qna.alt.markdown = "<p>Testing Markdown Sanitization</p>";
+        expect(store.dispatch).toHaveBeenNthCalledWith(1, 'api/check', 'test');
+        expect(store.dispatch).toHaveBeenNthCalledWith(2, 'data/add', {
+                "a": "test",
+                "alt": {
+                    "markdown": "<p>Testing Markdown Sanitization</p>"
+                },
+                "q": ["test"],
+                "qid": "test",
+                "type": "qna"
+        });
+        
+    });
+
+    test('add -- markdown sanitized makes multiple passes', async () => {
+        store.dispatch.mockReturnValueOnce(Promise.resolve(false));
+        const wrapper = mount(add, {
+            global: {
+                mocks: {
+                    $store: store,
+                },
+            },
+        });
+
+        wrapper.vm.$data.data = {
+            qna: {
+                qid:"test",
+                q:["test"],
+                a:"test",
+                alt:{
+                    markdown:"<scr<scr<badTag></badTag>ipt>"
+                },
+                type:"qna"
+            }
+        };
+        await wrapper.vm.add();
+
+        wrapper.vm.$data.data.qna.alt.markdown = "";
+        expect(store.dispatch).toHaveBeenNthCalledWith(1, 'api/check', 'test');
+        expect(store.dispatch).toHaveBeenNthCalledWith(2, 'data/add', {
+                "a": "test",
+                "alt": {
+                    "markdown": "ipt>"
+                },
+                "q": ["test"],
+                "qid": "test",
+                "type": "qna"
+        });
+        
+    }); 
+
+
 });
