56dd9e63773e205dbfa842539dd6c8e2c12ca472
Michael Lin <mikemlin@amazon.com>
2025-07-24 13:33:38 -0400

    Update to version v7.0.7

--- source/cli/aws_solutions/qnabot/cli/qnabot_cli.py
+++ source/cli/aws_solutions/qnabot/cli/qnabot_cli.py
@@ -15,7 +15,7 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.pass_context
 def cli(ctx) -> None:
     os.environ["SOLUTION_ID"] = "SO0189"
-    os.environ["SOLUTION_VERSION"] = "v7.0.6"
+    os.environ["SOLUTION_VERSION"] = "v7.0.7"
 
 
 @cli.command("import")
